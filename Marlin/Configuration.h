2514053b07987d7c40c8a733dd07fd528996b5a0
Enrique Walter Philippeaux <enriquewph@gmail.com>
2024-03-30 00:23:05 -0300

    name & uuid setup

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -63,7 +63,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Phr3d13, Geeetech A10 default)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "Enrique Walter Philippeaux" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -138,11 +138,11 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Geeetech A10"
+#define CUSTOM_MACHINE_NAME "HellCNC"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
-//#define MACHINE_UUID "00000000-0000-0000-0000-000000000000"
+#define MACHINE_UUID "ad6da323-885e-4cab-a7b0-0f43054ab8fd"
 
 // @section stepper drivers
 
